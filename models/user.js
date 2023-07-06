708250d8f9e7c24f1d84d3aad8149db507e2608d
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-06 15:30:13 +0330

    editing details of user model.

--- models/user.js
+++ models/user.js
@@ -3,14 +3,42 @@ const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    isAdmin: Boolean
+});
 
-userSchema.methods.generateAuthToken = function() {}
+userSchema.methods.generateAuthToken = function() {
+    return jwt.sign({_id: this._id, isAdmin: this.isAdmin}, config.get('jwtPrivateKey'));
+}
 
 const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
-    const schema = {};
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
     return Joi.validate(user, schema);
 }
 
