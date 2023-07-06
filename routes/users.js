b5db89d498e29dcdf0600e519445dfdc7005af20
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-06 15:43:20 +0330

    implement authentication for user.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,26 @@
 const auth = require('../middleware/auth');
-const user = require('../models/user');
+const {User, validate} = require('../models/user');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
 router.get('/me', auth, async (req, res) => {}); // authorization
 
-router.post('/', async (req, res) => { }); // authentication
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+
+    const token = user.generateAuthToken();
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+}); // authentication
 
 module.exports = router;
