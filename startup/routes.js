129d939b502c04db169810a4767151a1054034c8
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-07 00:01:16 +0330

    load airplane ticket router.

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const users = require('../routes/users')
+const airplaneTickets = require('../routes/airplaneTickets')
 // other routes
 const error = require('../middleware/error');
 
 module.exports = function(app) {
     app.use(express.json());
     app.use('/api/users', users);
+    app.use('api/airplane-tickets', airplaneTickets)
     app.use(error);
 };
 
