af0af7608255d6d5b0adeea109e2f42925ba1c76
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-07 15:33:08 +0330

    debugged and another get handler added.

--- routes/airplaneTickets.js
+++ routes/airplaneTickets.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 const {AirplaneTicket, validate} = require('../models/airplaneTicket');
 const isAdmin = require('../middleware/admin');
 const isVip = require('../middleware/vip');
 const auth = require('../middleware/auth');
 
-router.get('/:id', async(req, res) => {
-    const ticket = await AirplaneTicket.findById(req.params._id);
+router.get('/find:id', async(req, res) => {
+    const ticket = await AirplaneTicket.findById(req.params.id);
+    if (!ticket) return res.status(404).send('The ticket with the given ID was not found.');
+
     res.send(ticket);
 });
 
-router.get('/including-vips', isVip, async(req, res) => {
+router.get('/including-vips', [auth, isVip], async(req, res) => {
     const tickets = await AirplaneTicket
         .find()
         .sort('price');
@@ -24,22 +27,11 @@ router.get('/without-vips', async(req, res) => {
     res.send(tickets);
 });
 
-router.post('/', [isAdmin, auth], async(req , res) => {
+router.post('/', [auth, isAdmin], async(req , res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-
-    let ticket = new AirplaneTicket({
-        origin: req.body.origin,
-        destination: req.body.destination,
-        airLine: req.body.airLine,
-        departureTime: req.body.departureTime,
-        arrivalTime: req.body.arrivalTime,
-        flightNumber: req.body.flightNumber,
-        terminal: req.body.terminal,
-        price: req.body.price,
-        quantity: req.body.quantity,
-        isVip: req.body.isVip
-        });
+    let ticket = new AirplaneTicket(_.pick(req.body, ['origin', 'destination', 'airLine', 'departureTime',
+                                                        'arrivalTime', 'flightNumber', 'terminal', 'price', 'quantity', 'isVip']));
     ticket = await ticket.save();
 
     res.send(ticket);
