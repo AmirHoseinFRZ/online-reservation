c2f23b526b5da71430952d9340a2fe69dc8c79a4
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-07 14:28:35 +0330

    post and delete handlers added.

--- routes/airplaneTickets.js
+++ routes/airplaneTickets.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {AirplaneTicket, validate} = require('../models/airplaneTicket');
+const isAdmin = require('../middleware/admin');
+const isVip = require('../middleware/vip');
 const auth = require('../middleware/auth');
 
 router.get('/:id', async(req, res) => {
@@ -8,7 +10,7 @@ router.get('/:id', async(req, res) => {
     res.send(ticket);
 });
 
-router.get('/including-vips', async(req, res) => {
+router.get('/including-vips', isVip, async(req, res) => {
     const tickets = await AirplaneTicket
         .find()
         .sort('price');
@@ -21,6 +23,35 @@ router.get('/without-vips', async(req, res) => {
         .sort('price');
     res.send(tickets);
 });
+
+router.post('/', [isAdmin, auth], async(req , res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let ticket = new AirplaneTicket({
+        origin: req.body.origin,
+        destination: req.body.destination,
+        airLine: req.body.airLine,
+        departureTime: req.body.departureTime,
+        arrivalTime: req.body.arrivalTime,
+        flightNumber: req.body.flightNumber,
+        terminal: req.body.terminal,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        isVip: req.body.isVip
+        });
+    ticket = await ticket.save();
+
+    res.send(ticket);
+})
+
+router.delete('/:id', [isAdmin, auth], async(req , res) => {
+    const ticket = await AirplaneTicket.findByIdAndRemove(req.params.id);
+
+    if (!ticket) return res.status(404).send('The ticket with the given ID was not found.');
+
+    res.send(ticket);
+})
 // other handlers
 
 module.exports = router;
