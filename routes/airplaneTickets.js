ec87ec7fc13359433050c1808376c6e61ed17c26
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-07 04:28:39 +0330

    all get methods added.

--- routes/airplaneTickets.js
+++ routes/airplaneTickets.js
@@ -3,9 +3,24 @@ const router = express.Router();
 const {AirplaneTicket, validate} = require('../models/airplaneTicket');
 const auth = require('../middleware/auth');
 
-router.get('/', auth, async(req, res) => {
-    const tickets = await AirplaneTicket.find();
+router.get('/:id', async(req, res) => {
+    const ticket = await AirplaneTicket.findById(req.params._id);
+    res.send(ticket);
+});
+
+router.get('/including-vips', async(req, res) => {
+    const tickets = await AirplaneTicket
+        .find()
+        .sort('price');
     res.send(tickets);
 });
+
+router.get('/without-vips', async(req, res) => {
+    const tickets = await AirplaneTicket
+        .find({isVip: false})
+        .sort('price');
+    res.send(tickets);
+});
+// other handlers
 
 module.exports = router;
