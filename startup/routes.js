8a3428f9bf0fff87c1dcf592773f29dbd399e3e1
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-07 21:02:41 +0330

    train ticket router added.

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const users = require('../routes/users')
-const airplaneTickets = require('../routes/airplaneTickets')
+const users = require('../routes/users');
+const airplaneTickets = require('../routes/airplaneTickets');
+const trainTickets = require('../routes/trainTickets');
 // other routes
 const error = require('../middleware/error');
 
@@ -8,6 +9,8 @@ module.exports = function(app) {
     app.use(express.json());
     app.use('/api/users', users);
     app.use('/api/airplane-tickets', airplaneTickets)
+    app.use('api/train-tickets', trainTickets())
+    // other routers
     app.use(error);
 };
 
