ac48c8dcd966f03e7f071b5b9918b4e86ff9198d
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-07-07 15:32:07 +0330

    post handler debugged.

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.post('/sign-up', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    user = new User(_.pick(req.body, ['name', 'email', 'password', 'isVip', 'isAdmin']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
